a3763e37f91162fb4ff0803287e335a184d15798
LuizDML <luiz.physics@gmail.com>
2026-03-17 17:35:49 -0300

    Add Uvicorn dependency and implement Financial Search API with embedding services

---
+++ api/services/embeddings.py
@@ -0,0 +1,25 @@
+from fastembed import TextEmbedding, SparseTextEmbedding, LateInteractionTextEmbedding
+from config.settings import settings
+
+class EmbeddingsService:
+    def __init__(self):
+        self.dense_model = TextEmbedding(settings.dense_model)
+        self.sparse_model = SparseTextEmbedding(settings.sparse_model) 
+        self.colbert_model = LateInteractionTextEmbedding(settings.colbert_model) 
+
+    def embed_query(self, query: str):
+        dense = list(self.dense_model.query_embed([query]))[0].tolist()
+        sparse = list(self.sparse_model.query_embed([query]))[0].as_object()
+        colbert = list(self.colbert_model.query_embed([query]))[0].tolist()
+        return dense, sparse, colbert
+    
+    
+        
+    def get_dense_embedding(self, text: str):
+        return list(self.dense_model.passage_embed([text]))[0].tolist()
+
+    def get_sparse_embedding(self, text: str):
+        return list(self.sparse_model.passage_embed([text]))[0].as_object()
+
+    def get_colbert_embedding(self, text: str):
+        return list(self.colbert_model.passage_embed([text]))[0].tolist()
